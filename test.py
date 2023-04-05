f05975c66f17506fe7102417c4935c8d6b71d0cd
viviaxenov <viviaxenov@gmail.com>
2023-04-05 15:56:34 +0300

    Added tests for adjoint operator

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from numpy.random import normal
+
 from jko_primal_dual import *
 
 N_x = 8
@@ -19,16 +21,19 @@ probl = Problem(
 primal_variables = probl.get_primal_variables()
 dual_variables = probl.get_dual_variables()
 
-u = primal_variables
-Au = probl.apply_A_pde(primal_variables)
+u = normal(size=primal_variables.shape)
+phi = normal(size=dual_variables[0].shape)
 
-phi = dual_variables[0]
-At_phi = probl.apply_At_pde(dual_variables)
+Au = probl.apply_A_pde(u)
+
+At_phi = probl.apply_At_pde((phi,))
 
 dot_in_primal = u.ravel() @ At_phi.ravel()
 dot_in_dual= Au.ravel() @ phi.ravel()
 
+
 print(dot_in_primal, dot_in_dual, np.abs(dot_in_primal - dot_in_dual))
+
 
 
 
